71559c6944b5cb7e11d95878330a93f732747493
LiboBearChen <50467944+LiboBearChen@users.noreply.github.com>
2020-11-04 11:45:24 -0500

    update

--- app.py
+++ app.py
@@ -1,6 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app=Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/posts')
+def posts():
+    return render_template('posts.html')
 
 @app.route('/home/users/<string:name>/posts/<int:id>')
 def hello(name,id):
