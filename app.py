4ef825776a614b14c61799cfb05364310e941124
LiboBearChen <50467944+LiboBearChen@users.noreply.github.com>
2020-11-05 11:30:26 -0500

    update

--- app.py
+++ app.py
@@ -2,13 +2,25 @@ from flask import Flask, render_template
 
 app=Flask(__name__)
 
+all_posts=[
+    {
+        'title': 'Post 1',
+        'content': 'This is the content of post 1.',
+        'author':'Libo'
+    },
+    {
+        'title': 'Post 2',
+        'content': 'This is the content of post 2.'
+    }
+]
+
 @app.route('/')
 def index():
     return render_template('index.html')
 
 @app.route('/posts')
 def posts():
-    return render_template('posts.html')
+    return render_template('posts.html',posts=all_posts)
 
 @app.route('/home/users/<string:name>/posts/<int:id>')
 def hello(name,id):
