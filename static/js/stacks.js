db2246f0c8777d7ba496f9fa4da34c3ab3d06e22
LiboBearChen <50467944+LiboBearChen@users.noreply.github.com>
2020-11-29 22:07:13 -0500

    update

--- static/js/stacks.js
+++ static/js/stacks.js
@@ -1,2 +1,16 @@
 //fetch data from github api
 var request=new XMLHttpRequest()
+request.open('GET','https://api.github.com/users/libobearchen/repos',true)
+request.onload=function(){
+    var data=JSON.parse(this.response)
+    var statusHTML='<table><thead><tr><th>Name</th><th>Language</th></tr></thead><tbody>'
+    $.each(data,function(i,status){
+        statusHTML+='<tr>'
+        statusHTML+='<td>'+status.name+'</td>'
+        statusHTML+='<td>'+status.language+'</td>'
+        statusHTML+='</tr>'
+    })
+    statusHTML+='</tbody></table>'
+    $('#fetchContainer').html(statusHTML)
+}
+request.send()
