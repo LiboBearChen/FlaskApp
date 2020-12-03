a31be0c2b7edf5968e3b39e75a8de1129161b994
LiboBearChen <50467944+LiboBearChen@users.noreply.github.com>
2020-12-02 21:35:18 -0500

    update

--- static/js/stacks.js
+++ static/js/stacks.js
@@ -1,26 +1,35 @@
 //fetch data from github api
-var request = new XMLHttpRequest()
-request.open('GET', 'https://api.github.com/repos/libobearchen/canvasgame/contents/index.html', true)
-request.onload = function () {
-    var data = JSON.parse(this.response)
-    var download_url = data.download_url
+function fetchData(stack) {
+    console.log(stack)
+    var request = new XMLHttpRequest()
+    var file = 'ssssssssss'
+    if (stack===1) {
+        file = 'index.html'
+    } else if(stack===2){
+        file = 'index.js'
+    } 
+    console.log(file)
+    request.open('GET', `https://api.github.com/repos/libobearchen/canvasgame/contents/${file}`, true)
+    request.onload = function () {
+        var data = JSON.parse(this.response)
+        var download_url = data.download_url
 
-    //down load source code
-    var request1 = new XMLHttpRequest()
-    request1.open('GET', download_url, true)
-    request1.onload = function () {
-        $('#fetchContainer').html(this.response)
+        //down load source code
+        var request1 = new XMLHttpRequest()
+        request1.open('GET', download_url, true)
+        request1.onload = function () {
+            let backString = '<plaintext>' + this.response.toString()
+            $('#fetchContainer').html(backString)
+        }
+        request1.send()
     }
-    request1.send()
+    request.send()
 }
-request.send()
 
-/* var statusHTML = '<table><thead><tr><th>Name</th><th>Language</th></tr></thead><tbody>'
-$.each(data, function (i, status) {
-    statusHTML += '<tr>'
-    statusHTML += '<td>' + status.name + '</td>'
-    statusHTML += '<td>' + status.language + '</td>'
-    statusHTML += '</tr>'
+$(document).ready(function(){
+    //radio buttons listener
+    $('input[type=radio][name=stack]').change(function() {
+        console.log(this.value)
+    })
+
 })
-statusHTML += '</tbody></table>'
-$('#fetchContainer').html(statusHTML) */
