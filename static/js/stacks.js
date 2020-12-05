42f3bf75cb8893707c0b685956c92e1cbe5a127a
LiboBearChen <50467944+LiboBearChen@users.noreply.github.com>
2020-12-04 20:09:21 -0500

    update

--- static/js/stacks.js
+++ static/js/stacks.js
@@ -3,11 +3,11 @@ function fetchData(stack) {
     $('#fetchContainer').html("<h3 style='text-align:center;'>Fetching data from Github...</h3>")
     var request = new XMLHttpRequest()
     var file = ''
-    if (stack==1) {
+    if (stack == 1) {
         file = 'index.html'
-    } else if(stack==2){
+    } else if (stack == 2) {
         file = 'index.js'
-    } 
+    }
     request.open('GET', `https://api.github.com/repos/libobearchen/canvasgame/contents/${file}`, true)
     request.onload = function () {
         var data = JSON.parse(this.response)
@@ -17,27 +17,38 @@ function fetchData(stack) {
         var request1 = new XMLHttpRequest()
         request1.open('GET', download_url, true)
         request1.onload = function () {
-            let backString = '<pre>' + colorTags(this.response.toString())+'</pre>'
+            let backString = '<pre>' + checkCode(this.response.toString()) + '</pre>'
             $('#fetchContainer').html(backString)
-            
+
         }
         request1.send()
     }
     request.send()
 }
 
-//color tags
-function colorTags(code){
-    let wordArray = code.match(/function/g)
-    console.log(wordArray)
+//function list template
+function ULTemplate(items, element) {
+    let resultsHTML = ""
+    if (items) {
+        for (i = 0; i < items.length - 1; i++) {
+            resultsHTML += "<li>Function " + (i + 1) + " : " + items[i] + "</li>"
+        }
+    }
+    element.innerHTML = resultsHTML
+}
+
+//analyse and change source code
+function checkCode(code) {
+    let functionArray = code.match(/function/g)
+    ULTemplate(functionArray, document.getElementById("functions"))
     let replaceText = "<mark id='markWord'>$&</mark>"
-    let returnStr=code.replace(/function/g, replaceText)
+    let returnStr = code.replace(/function/g, replaceText)
     return returnStr
 }
 
-$(document).ready(function(){
+$(document).ready(function () {
     //radio buttons listener
-    $('input[type=radio][name=stack]').change(function() {
+    $('input[type=radio][name=stack]').change(function () {
         fetchData(this.value)
     })
 
