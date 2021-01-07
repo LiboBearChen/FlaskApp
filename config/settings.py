44fa97e42057cc73dc11685485ef1bcaa8727b0e
LiboBearChen <50467944+LiboBearChen@users.noreply.github.com>
2021-01-07 18:24:03 -0500

    update

--- config/settings.py
+++ config/settings.py
@@ -1,7 +1,7 @@
 #local database
-#SQLALCHEMY_DATABASE_URI = 'mysql://root:password@localhost:3306/liboblog'
+SQLALCHEMY_DATABASE_URI = 'mysql://root:password@localhost:3306/liboblog'
 
 #remote database
-SQLALCHEMY_DATABASE_URI = 'mysql://hiz5a4nuqy4s0z4v:rci7yarosqu09ntk@jhdjjtqo9w5bzq2t.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/yfcbmlfvwawpomaw'
+#SQLALCHEMY_DATABASE_URI = 'mysql://hiz5a4nuqy4s0z4v:rci7yarosqu09ntk@jhdjjtqo9w5bzq2t.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/yfcbmlfvwawpomaw'
 
 SQLALCHEMY_COMMIT_ON_TEARDOWN = True
