bc5fc1c8742b611e4daa80343a638e1e5b65a6a6
LiboBearChen <50467944+LiboBearChen@users.noreply.github.com>
2021-02-15 17:40:58 -0500

    update

--- blog/static/js/projects.js
+++ blog/static/js/projects.js
@@ -1,23 +1,27 @@
 //change active tabs and show the right content
 function changeActive(id) {
-    //make ids for tabs and contents
-    let tabID = '#tab' + id;
-    let target = document.querySelector(tabID);
-    target.click()
+  //make ids for tabs and contents
+  let tabID = "#tab" + id;
+  let target = document.querySelector(tabID);
+  target.click();
 }
 
 //run function after the page loaded completely
-document.addEventListener('DOMContentLoaded', function () {
+document.addEventListener(
+  "DOMContentLoaded",
+  function () {
     //get variable from url
-    let pathArray = window.location.pathname.split('/');
+    let pathArray = window.location.pathname.split("/");
     let parameter = pathArray[pathArray.length - 1];
     changeActive(parameter);
-}, false);
+  },
+  false
+);
 
 //generate org charts
 $(function () {
-    $('#chart1').orgChart({ container: $('#chartContainer1') });
-    $('#chart2').orgChart({ container: $('#chartContainer2') });
-    $('#chart3').orgChart({ container: $('#chartContainer3') });
-    $('#chart4').orgChart({ container: $('#chartContainer4') });
-})
+  $("#chart1").orgChart({ container: $("#chartContainer1") });
+  $("#chart2").orgChart({ container: $("#chartContainer2") });
+  $("#chart3").orgChart({ container: $("#chartContainer3") });
+  $("#chart4").orgChart({ container: $("#chartContainer4") });
+});
